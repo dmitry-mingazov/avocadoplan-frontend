2a9aa447336d5ba7ed082fa6e5af9bcfc9cd5d52
dmitry-mingazov <mingazov@email.com>
2020-01-20 17:15:29 +0100

    [DELETE] Form components

--- src/app/pages/create/create.module.ts
+++ src/app/pages/create/create.module.ts
@@ -7,9 +7,6 @@ import { IonicModule } from '@ionic/angular';
 import { CreatePageRoutingModule } from './create-routing.module';
 
 import { CreatePage } from './create.page';
-import { DayFormComponent } from 'src/app/_forms/day-form/day-form.component';
-import { MealFormComponent } from 'src/app/_forms/meal-form/meal-form.component';
-import { DishFormComponent } from 'src/app/_forms/dish-form/dish-form.component';
 import { PipesModule } from 'src/app/_pipes/pipes/pipes.module';
 import { EnumToArrayPipe } from 'src/app/_pipes/enum-to-array.pipe';
 
@@ -23,9 +20,6 @@ import { EnumToArrayPipe } from 'src/app/_pipes/enum-to-array.pipe';
 
   ],
   declarations: [CreatePage,
-    DayFormComponent,
-    MealFormComponent,
-    DishFormComponent,
     EnumToArrayPipe
     ]
 })
