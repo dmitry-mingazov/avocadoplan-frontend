a930acf26b32630f1c8e059b2a7f4f5244eaf9bf
dmitry-mingazov <mingazov@email.com>
2020-02-07 17:40:07 +0100

    [UPDATE] - Prettified homepage, it shows requested plans

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { PlanService } from 'src/app/services/plan.service';
 import { Plan } from 'src/app/_interfaces/plan.interface';
@@ -8,16 +8,22 @@ import { Plan } from 'src/app/_interfaces/plan.interface';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
   private plans: Plan[] = null;
+  private loading: boolean = true;
 
   constructor(
     private planService: PlanService
   ) {}
 
+  ngOnInit() {
+    this.getHome();
+  }
+
   getHome() {
     this.planService.getHome().subscribe( (data) => {
       this.plans = data;
+      this.loading = false;
     })
   }
 
